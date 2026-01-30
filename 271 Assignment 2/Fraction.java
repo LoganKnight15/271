7ade36ff08ed1b9b48349f7d5e6451445ee2aa63
LoganKnight15 <robinsonr19@mailbox.winthrop.edu>
2026-01-30 12:46:53 -0500

    Basic Fraction layout added

--- 271 Assignment 2/Fraction.java
+++ 271 Assignment 2/Fraction.java
@@ -1,3 +1,18 @@
 public class Fraction {
-
+    private long numerator;
+    private long denominator;
+    public Fraction(long num, long denom){
+    /* your constructor logic here */
+    }
+    // example constructor case
+    public Fraction(long num){
+    numerator = num;
+    denominator = 1;
+    }
+    public long getNumerator() {
+    /* your logic here */
+    }
+    public long getDenominator() {
+    /* your logic here */
+    }
 }
