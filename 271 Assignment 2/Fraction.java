f6ca9778c9cbe0b74a66ee44b62f4bfe52d1a640
LoganKnight15 <robinsonr19@mailbox.winthrop.edu>
2026-02-05 07:57:59 -0500

    small spacing changes

--- 271 Assignment 2/Fraction.java
+++ 271 Assignment 2/Fraction.java
@@ -2,7 +2,7 @@ public class Fraction implements FractionInterface{
 
     private long numerator;
     private long denominator;
-    
+
     public Fraction(long num, long denom){
     /* your constructor logic here */
     }
@@ -11,32 +11,32 @@ public class Fraction implements FractionInterface{
     numerator = num;
     denominator = 1;
     }
-    public long getNumerator() {
+    public long getNumerator(){
     /* your logic here */
     }
-    public long getDenominator() {
+    public long getDenominator(){
     /* your logic here */
     }
-    public Fraction add(Fraction inFraction) {
+    public Fraction add(Fraction inFraction){
     /* your logic here */
     }
-    public Fraction subtract(Fraction inFraction) {
+    public Fraction subtract(Fraction inFraction){
     /* your logic here */
     }
-    public Fraction multiply(Fraction inFraction) {
+    public Fraction multiply(Fraction inFraction){
     /* your logic here */
     }
-    public Fraction divide(Fraction inFraction) {
+    public Fraction divide(Fraction inFraction){
     /* your logic here */
     }
-    public Fraction pow(int n) {
+    public Fraction pow(int n){
     /* your logic here */
     }
-    public Fraction negate() {
+    public Fraction negate(){
     /* your logic here */
     }
     @Override
-    public String toString() {
+    public String toString(){
     /* put your print logic here */
     }
 
