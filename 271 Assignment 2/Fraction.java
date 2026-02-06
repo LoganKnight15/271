fa5867ab72c8c647a6bf56fd43b1b453aefc179b
LoganKnight15 <robinsonr19@mailbox.winthrop.edu>
2026-02-06 11:53:55 -0500

    Added programming standards

--- 271 Assignment 2/Fraction.java
+++ 271 Assignment 2/Fraction.java
@@ -1,3 +1,16 @@
+/*************************************************************************
+* Project 2 for CSCI 271-001 Spring 2026
+*
+* Author: Logan Robinson
+* OS: MacOs
+* Compiler: javac 21.0.9
+* Date: Feburary 6, 2026
+*
+* Purpose
+* This program creates, multiples, adds, subtracts, divides, rasies to powers, and negates Fractions.
+* It will also convert them to strings for output purposes.
+*
+*************************************************************************/
 public class Fraction implements CSCI271_Assignemnt2_FractionInterface{
 
     private long numerator;
