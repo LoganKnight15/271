8a817409fed91f4e17744085bf3bd67e7a7720fc
LoganKnight15 <robinsonr19@mailbox.winthrop.edu>
2026-02-06 11:46:18 -0500

    Added Comments

--- 271 Assignment 2/Assignment2.java
+++ 271 Assignment 2/Assignment2.java
@@ -1,16 +1,18 @@
 public class Assignment2 {
     public static void main(String[] args){
         /*
-        Tasks 1 and 2 test code for creating and printing fractions. Fractions a and b are created properly. Printing will result in b outpouting just 0 however it
-        does create the fraction as 0/1. Chose to do this two together as it made the most sense development wise to have the creation and the output of the fractions be linked
+        Tasks 1 and 2 test code for creating and printing fractions. Fractions a and b are created properly. 
+         Chose to do this two together as it made the most sense development wise to have the 
+         creation and the output of the fractions be linked. These cases are taken directly from the instruction page
+         fr1 -fr5 are testing the toString() fucntion and there cases are shown.
         */
-        Fraction a = new Fraction( 6, -24); // creates a fraction of numerator = -1 and denominator = 4
-        Fraction b = new Fraction( 0, 8 );// creates a fraction of numerator = 0, denominator = 1 (not 8)
-        System.out.println(a);
-        System.out.println(b);
+        Fraction a1 = new Fraction( 6, -24); // creates a fraction of numerator = -1 and denominator = 4
+        Fraction b1 = new Fraction( 0, 8 );// creates a fraction of numerator = 0, denominator = 1 (not 8)
+        System.out.println(a1);
+        System.out.println(b1);
 
-        Fraction fr = new Fraction(8, -6); // fr.toString() is "-4/3"
-        System.out.println(fr);
+        Fraction fr1 = new Fraction(8, -6); // fr.toString() is "-4/3"
+        System.out.println(fr1);
         Fraction fr2 = new Fraction(23, 0); // fr.toString() is "Infinity"
         System.out.println(fr2);
         Fraction fr3 = new Fraction(-6, 0); // fr.toString() is "-Infinity"
@@ -21,23 +23,94 @@ public class Assignment2 {
         System.out.println(fr5); 
 
         /*
-        Code for Task 3 test cases
+        Code for Task 3 test cases are listed below
+
+        1st we start with three cases for addition. The first is just adding two positive numbers. 3/5 and 7.
+        The second addition case is tetsing adding a negative and postive fraction. The last acse is adding two negative
+        fractions together. The same can be seen for the subtraction, multiplication, and division cases. I have added
+        the outputs and inital fractions above each test case for easer reading. These cases were mathmatically tested
+        using a calculator to check for proper addition, subtraction, multiplication, and division.
+
+        Following the add, sub, multi, and divide section comes power and negate. Power has 3 test cases listed showing
+        a positive power, negative power, and a zero power.
+
+        Negate has 4 test cases showing Infinity switches, normal fraction cases, and NaN
         */
-       Fraction r = new Fraction(3,5).add(new Fraction(7)); //Outputs 38/5
-       System.out.println(r);
-       Fraction t = new Fraction(76,30).multiply(new Fraction(8, 13)); //Outputs 304/195
-       System.out.println(t);
-       Fraction d = new Fraction(4,21).divide(new Fraction(3,85)); //Outputs 340/63
-       System.out.println(d);
-       Fraction s = new Fraction(21, 8).subtract(new Fraction(80,11)); //Outputs -409/88
-       System.out.println(s);
-       Fraction p = new Fraction(1, 4).pow(3); //Outputs 1/64
+        //The addition of the fractions 3/5 and 7/1 which outputs 38/5
+       Fraction addp = new Fraction(3,5).add(new Fraction(7));
+       System.out.println(addp);
+        //The addition of the fractions 7/14 and -20/50 which outputs 1/10
+       Fraction addn = new Fraction(7,14).add(new Fraction(-20,50));
+       System.out.println(addn);
+        //The addition of the fractions -7/46 and -2/5 which outputs -127/230
+       Fraction addn2 = new Fraction(-7,46).add(new Fraction(-2,5));
+       System.out.println(addn2);
+        //The multiplication of the fractions 76/30 and 8/13 which outputs 304/195
+       Fraction mp = new Fraction(76,30).multiply(new Fraction(8, 13));
+       System.out.println(mp);
+        //The multiplication of the fractions 7/3 and -80/100 which outputs -28/15
+       Fraction mn = new Fraction(7,3).multiply(new Fraction(-80, 100));
+       System.out.println(mn);
+        //The multiplication of the fractions -40/3 and -5/20 which outputs 10/3
+       Fraction mn2 = new Fraction(-40,3).multiply(new Fraction(-5, 20));
+       System.out.println(mn2);
+        //The division of the fractions 4/21 and 3/85 which outputs 340/63
+       Fraction dp = new Fraction(4,21).divide(new Fraction(3,85));
+       System.out.println(dp);
+        //The division of the fractions 100/74 and -9/85 which outputs -4250/33
+       Fraction dn = new Fraction(100,74).divide(new Fraction(9,-85));
+       System.out.println(dn);
+        //The division of the fractions -4/210 and -33/8 which outputs 16/3465
+       Fraction dn2 = new Fraction(-4,210).divide(new Fraction(-33,8));
+       System.out.println(dn2);
+        //The subtraction of the fractions 21/8 and 80/11 which outputs -409/88
+       Fraction sp = new Fraction(21, 8).subtract(new Fraction(80,11));
+       System.out.println(sp);
+        //The subtraction of the fractions 2/83 and -5/11 which outputs 437/913
+       Fraction sn = new Fraction(2, 83).subtract(new Fraction(-5,11));
+       System.out.println(sn);
+        //The subtraction of the fractions -7/5000 and -3/2 which outputs 7493/5000
+       Fraction sn2 = new Fraction(-7, 5000).subtract(new Fraction(-3,2));
+       System.out.println(sn2);
+        //The fraction 1/4 raised to the pwoer of 3 which is 1/64
+       Fraction p = new Fraction(1, 4).pow(3);
        System.out.println(p);
-       Fraction pn = new Fraction(1, 4).pow(-3); //Outputs 64
+       //The fraction 1/4 raised to the power of -3 which is 64
+       Fraction pn = new Fraction(1, 4).pow(-3);
        System.out.println(pn);
+       //The fraction 1/4 raised to the pwoer of 0 which is 1
        Fraction pz = new Fraction(1, 4).pow(0); //Outputs 1
        System.out.println(pz);
-       Fraction ne = new Fraction(1,0).negate();
+        //The negation of the fraction 0/0 which is normally NaN showing NaN negated.
+       Fraction ne = new Fraction(0,0).negate();
        System.out.println(ne);
+        //The negation of the fraction 1/0 which is normnally Infinity showing -Infinity negated.
+       Fraction nei = new Fraction(1,0).negate();
+       System.out.println(nei);
+        //The negation of the fraction -2/0 which is normally -infinity showing Infinity negated.
+       Fraction neni = new Fraction(-2,0).negate();
+       System.out.println(neni);
+        //The negation of the fraction 7/50 which is -7/50 negated;
+       Fraction nep = new Fraction(7,50).negate();
+       System.out.println(nep);
+        //The negation of the fraction -55/30 which is reduced to 11/6 when negated.
+       Fraction nen = new Fraction(-55,30).negate();
+       System.out.println(nen);
+
+       /*
+       The below code is the big test case you showed in the instruction page being tested with
+       my own verison following it.
+        */
+        Fraction a2 = new Fraction(16); // 16/1
+        Fraction b2 = new Fraction(3,5).add(new Fraction(7)); // 3/5 + 7/1 = 38/5
+        Fraction c2 = new Fraction(6,7); // 6/7
+        Fraction results = c2.multiply(a2.divide(b2)); //Outputs 240/133
+        System.out.println(results);
+        //My own version
+        Fraction a3 = new Fraction(10, 26); // 10/26
+        Fraction b3 = new Fraction(9,-60).subtract(new Fraction(7, 39)); // -9/60 - 7/39
+        Fraction c3 = new Fraction(2,7); // 2/7
+        Fraction result = c3.divide(a3.multiply(b3)); // Outputs -4056/1799
+        System.out.println(result);
     }
 }
--- 271 Assignment 2/Fraction.java
+++ 271 Assignment 2/Fraction.java
@@ -2,7 +2,11 @@ public class Fraction implements FractionInterface{
 
     private long numerator;
     private long denominator;
-
+    /* The fraction function takes in a numerator and denominator and asssigns them to numerator and denominator. We then
+    call the gcd function and once we get the gcd we dvide both teh numerator and denominator by this. We also check if the
+    denominator is negative if so we flip the values of the numerator from postive to negative and denominator from 
+    negative to positive to keep with the format -a/b for a fraction.
+     */
     public Fraction(long num, long denom){
         numerator = num;
         denominator = denom;
@@ -16,13 +20,16 @@ public class Fraction implements FractionInterface{
         }
     }
 
-
+    //Fraction function for if we have only a numerator. Denominator is assumed to be 1 in this case
     public Fraction(long num){
         numerator = num;
         denominator = 1;
     }
 
-
+    /*gcd takes in the numerator and denominator of the new fraction and finds there greatest common denominator
+    it cheks if a is negative is so it negates it. Then it assignes a with the gcd. if a - 0 then a is assigned with 1.
+    lastly it returns a.
+     */
     public static long gcd(long a, long b){
         if ( a < 0 ){
             a = -a;
@@ -39,28 +46,35 @@ public class Fraction implements FractionInterface{
     }
 
 
-    @Override
+    //Returns the numerator
     public long getNumerator(){
         return numerator;
     }
 
 
-    @Override
+    //Returns the Denominator
     public long getDenominator(){
         return denominator;
     }
 
-
+    /*the divide function takes in a fraction to divide the one its called by with. it uses getNumerator
+    and getDenominator to assign num and denum. newnum is set equal to the numerator of the first multlied with
+    the denominator of the second and newdenum is set euqal to the denominator of both the first multipled
+    with the numerator of the second. Lastly we return a new fraction.
+     */
     public Fraction divide(Fraction inFraction){
         //While not neccessary used this here to actually use the getNumerator and getDenomiator functions
         long num = getNumerator();
         long denum = getDenominator();
-        long newnum = num * inFraction.getDenominator();
-        long newdenum = denum * inFraction.getNumerator();
+        long newnum = num * inFraction.denominator;
+        long newdenum = denum * inFraction.numerator;
         return new Fraction(newnum, newdenum);
     }
 
-
+    /*the multiply function takes in a fraction to multiply the one its called by with. it uses getNumerator
+    and getDenominator to assign num and denum. newnum is set equal to the numerators of both multlied togther and
+    newdenum is set euqal to the denominators of both fractions multipled together. Lastly we return a new fraction.
+     */
     public Fraction multiply(Fraction inFraction){
         long num = getNumerator();
         long denum = getDenominator();
@@ -69,7 +83,11 @@ public class Fraction implements FractionInterface{
         return new Fraction(newnum, newdenum);
     }
 
-
+    /*the addition function takes in a fraction to add to the one its called by with. It uses getNumerator and getDenominator
+    to assign num and denum. newnum s assigned with the numerator of the first times the denominator of the second
+    added with the denominator of the first times the numerator of the second. newdenum is both denominators multiplied.
+    Lastly a new fraction is returned.
+     */
     public Fraction add(Fraction inFraction){
         long num = getNumerator();
         long denum = getDenominator();
@@ -78,7 +96,11 @@ public class Fraction implements FractionInterface{
         return new Fraction(newnum, newdenum);
     }
 
-
+    /*The subtract function take in a fraction to subtract the one its called by with. It uses getNumerator and
+    getDenominator to assign num and denum with their respective values. newnum is set equal to the num of the
+    first fraction times the denominator of the second dubtracted by the denominator ofn the first times the
+    numerator of the second. newdenum is the two denominators multiplied together. Lastly we return a new fraction
+     */
     public Fraction subtract(Fraction inFraction){
         long num = getNumerator();
         long denum = getDenominator();
@@ -87,7 +109,13 @@ public class Fraction implements FractionInterface{
         return new Fraction(newnum, newdenum);
     }
 
-
+    /*The pow function takes in a int n power. It uses getNumerator and get Denominator to assign the values of num
+    and denum. two new variables newnum and newdenum are assigned with thee inital values of num and denum
+    afterwards powe is set equal to the absoluteb value of n. We then check if n = 0. if so then newnum and denum are
+    both set equal to 1. else we use a for loop to check lop the number of powe time. with newnum being setequal to itself
+    times num and the same with denum. After that we look to see if n was negative aka n < 0 if so we flip the values
+    of newnum and newdenum. Lastly we return a new fraction with the newnum and newdenum values
+    */
     public Fraction pow(int n){
         long num = getNumerator();
         long denum = getDenominator();
@@ -112,7 +140,8 @@ public class Fraction implements FractionInterface{
         return new Fraction(newnum, newdenum);
     }
 
-
+    //The negate function calls getNumerator and getDenominator to assign the variable num and denum
+    // num. is then set equal to its negation and then a new fraction is returned
     public Fraction negate(){
         long num = getNumerator();
         long denum = getDenominator();
@@ -120,7 +149,12 @@ public class Fraction implements FractionInterface{
         return new Fraction(num, denum);
     }
 
-
+    /*The toString function uses getNumerator and getDenominator to asign num and denum
+    We then check if denum = 0 and num > 0 if so the answer is infinity. Next if denum is still 0 but the numerator
+    is negative with say negative infinity. Next if the denominator is 1 we just output the numerator. Next if both
+    num and denum are 0 we say its not a number of NaN. If none of those apply we out put the numerator followed by
+    a / and then the denominator. Lastly returning the output.
+    */
     @Override
     public String toString(){
         String output = " ";
