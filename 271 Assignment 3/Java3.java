05569bd54495aa2a642f0cd2b64b5c71b71cd8b1
LoganKnight15 <robinsonr19@mailbox.winthrop.edu>
2026-02-25 14:01:30 -0500

    3 4 5 and 6

---
+++ 271 Assignment 3/Java3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Java3{
+
+    public static int maxInt(){
+
+    }
+
+    public static void main(String[] args) {
+        int userLength;
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Please state how any integers you are going to enter");
+        userLength = userInput.nextInt();
+
+        int arr[] = new int[userLength];
+
+        System.out.println("Please enter a list of integers");
+        
+    }
+}
